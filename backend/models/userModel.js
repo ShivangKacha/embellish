3dbe4d7d73086908cd749f268bac84fd5def1b71
ShivangKacha <shivangkachasbk9@gmail.com>
2024-06-26 22:09:01 +0530

    created models

---
+++ backend/models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPass) {
+  //compare the entered password and password stored in database for that user
+  return await bcrypt.compare(enteredPass, this.password);
+};
+
+// Encrypt password using bcrypt before storing in database
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
