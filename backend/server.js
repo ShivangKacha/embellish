99415eaf52bfc64a40c38acc83a19d04a06e0ee4
ShivangKacha <shivangkachasbk9@gmail.com>
2024-06-26 22:08:09 +0530

    created backend server

---
+++ backend/server.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookie parser middleware
+app.use(cookieParser());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/payment", paymentRoutes);
+
+// app.get("/api/config/paypal", (req, res) =>
+//   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+// );
+
+const dirname = path.resolve(); //set __dirname to current directory
+app.use("/uploads", express.static(path.join(dirname, "/uploads")));
+
+if (process.env.NODE_ENV == "production") {
+  const __dirname = path.resolve();
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running...");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
