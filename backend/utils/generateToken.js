526e813b529f29292ff9804e4506d72b509c9856
ShivangKacha <shivangkachasbk9@gmail.com>
2024-06-27 13:12:41 +0530

    jwt testing

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,11 +4,11 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
-
+  console.log(token)
   // Set JWT as an HTTP-Only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    // secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
     // sameSite: "strict", // Prevent CSRF attacks
     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
   });
