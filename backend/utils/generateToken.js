7fe453ab210aece40bc971a797ce16d1874022b3
ShivangKacha <shivangkachasbk9@gmail.com>
2024-06-27 13:48:16 +0530

    jwt secure site

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    // sameSite: "strict", // Prevent CSRF attacks
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    sameSite: "strict", // Prevent CSRF attacks
     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
   });
 };
