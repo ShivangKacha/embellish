08fb8131c29586cebb8adf6536e97c53271c7443
ShivangKacha <shivangkachasbk9@gmail.com>
2024-06-28 17:45:35 +0530

    generate token updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,11 +6,19 @@ const generateToken = (res, userId) => {
   });
   console.log(token)
   // Set JWT as an HTTP-Only cookie
+  // res.cookie("jwt", token, {
+  //   httpOnly: false,
+  //   secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+  //   sameSite: "strict", // Prevent CSRF attacks
+  //   maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+  // });
   res.cookie("jwt", token, {
-    httpOnly: false,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    httpOnly: true, // Set to true to prevent access by JavaScript
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
     sameSite: "strict", // Prevent CSRF attacks
     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+    domain: process.env.COOKIE_DOMAIN || undefined, // Set domain if needed
+    path: '/', // Set path if needed
   });
 };
 
